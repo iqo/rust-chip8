9291248ffe882719b381b2ce3ccc31dfc93c7215
iqo <niklas.brynolf@gmail.com>
2023-02-21 19:08:22 +0100

    cpu fix

--- src/cpu.rs
+++ src/cpu.rs
@@ -7,6 +7,8 @@ pub struct Cpu {
     vx: [u16; 16],
     i: u16,
     pc: u16,
+    sp:u8,
+    stack: [u16;16],
     rng: ThreadRng,
     
 
@@ -17,7 +19,9 @@ impl Cpu {
         return Cpu {
             vx: [0; 16],
             i: 0,
-            pc: 0,
+            pc: 0x200,
+            sp: 0,
+            stack: [0; 16],
             rng: rand::thread_rng(),
         };
     }
