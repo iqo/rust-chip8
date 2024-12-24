80f4da1a18840462433a0fffe54052a07436e3e9
Niklas Brynolf <niklas.brynolf@gmail.com>
2024-12-24 16:52:34 +0100

    register

--- chip8/src/bus/registers.rs
+++ chip8/src/bus/registers.rs
@@ -1,3 +1,24 @@
+fn op_00e0_cls() {
+
+}
+
+fn op_00ee_ret() {
+
+}
+
+fn op_1nnn_jp_addr() {
+
+}
+
+fn op_2nnn_call_addr() {
+
+}
+
+fn op_3xkk_se_vx_byte() {
+
+}
+
+
 #[cfg(test)]
 #[path ="./registers_test.rs"]
 mod registers_test;
