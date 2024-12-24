f6487be6124b3d92e84ee8b08898082ce5afc8e1
Niklas Brynolf <niklas.brynolf@gmail.com>
2024-12-24 23:00:01 +0100

    nibble

--- chip8/src/cpu.rs
+++ chip8/src/cpu.rs
@@ -1,8 +1,8 @@
-use crate::{bus::{ram::Ram, vram::Vram}, constants::PROGRAM_START};
+use crate::{bus::{ram::Ram, vram::Vram}, constants::{OPCODE_SIZE, PROGRAM_START}};
 enum ProgramCounter {
     Next,
     Skip,
-    Jump(usize)
+    Jump(u16)
 }
 impl ProgramCounter {
     fn skip_if(condition: bool) -> ProgramCounter {
@@ -40,11 +40,36 @@ impl Cpu {
     pub fn write_register(&mut self, index: usize, value: u8) {
         self.v[index] = value;
     }
-    
+
     pub fn read_register(&self, index: usize,) -> u8 {
         return self.v[index];
     }
 
+    pub fn run_opcode (&mut self, opcode: u16) {
+        let nibbles = (
+            (opcode & 0xF000) >> 12 as u8,
+            (opcode & 0x0F00) >> 8 as u8,
+            (opcode & 0x00F0) >> 4 as u8,
+            (opcode & 0x000F) as u8,
+        );
+        let nnn = (opcode & 0x0FFF) as usize;
+        let kk = (opcode & 0x00FF) as u8;
+        let x = nibbles.1 as usize;
+        let y = nibbles.2 as usize;
+        let n = nibbles.3 as usize;
+        let program_counter_change = match nibbles {
+            (0x00, 0x00, 0x0e, 0x00) => self.op_00e0(),
+                _ => ProgramCounter::Next,
+            };
+            match program_counter_change {
+                ProgramCounter::Next => self.program_counter += OPCODE_SIZE,
+                ProgramCounter::Skip => self.program_counter += 2 * OPCODE_SIZE,
+                ProgramCounter::Jump(addr) => self.program_counter = addr,
+        }
+    } 
+    fn op_00e0(&self) -> ProgramCounter {
+        return ProgramCounter::Next;
+    }
 }
 #[cfg(test)]
 #[path ="./cpu_test.rs"]
